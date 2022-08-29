9d2689b8f9b8e882f682eda3b5c356da6160f038
Abhilash <abhilashkm37@gmail.com>
2022-08-29 18:23:07 +0530

    hiii

---
+++ Person.cs
@@ -0,0 +1,53 @@
+﻿namespace Batch1_DET_2022
+{
+    internal class Person
+    {
+        private string v1;
+        private string v2;
+        private string v3;
+        private DateOnly dateOnly;
+
+        public Person(string v1, string v2, string v3, DateOnly dateOnly)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v3 = v3;
+            this.dateOnly = dateOnly;
+        }
+
+        internal string BDayStatus()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal int BirthYear()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal string ChineseZodiac()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal string DefaultUsername()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal string GetInfo()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal string GetSunSign()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal string IsAdult()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
